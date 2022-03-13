2081a6e295a89e565a4693a8785262cae06be900
Mu-An Chiou <me@muanchiou.com>
2022-03-13 17:49:12 +0800

    more notes from robb

--- Sources/ab-swift/main.swift
+++ Sources/ab-swift/main.swift
@@ -1,5 +1,5 @@
 let numbers: [Character] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let answer = String(numbers.shuffled().prefix(3))
+let answer = String(numbers.shuffled().prefix(4))
 var guess = ""
 
 while guess != answer {
@@ -14,23 +14,12 @@ while guess != answer {
 print("gj")
 
 func compare(guess: String) -> String {
-    var aCount = 0
-    var bCount = 0
-    var guessed: [String] = []
-    for indexInGuess in guess.indices {
-        let num = String(guess[indexInGuess])
-        let indexInAnswer = answer.firstIndex(of: guess[indexInGuess])
-        if indexInAnswer == nil { continue }
-        if indexInGuess == indexInAnswer {
-            if (guessed.contains(num)) {
-                bCount -= 1
-            }
-            aCount += 1
-        } else if !guessed.contains(num) {
-            bCount += 1
+    let correctPositions = zip(guess, answer).filter(==).count
+    let correctDigits = Set(guess)
+        .map { character in
+            answer.filter { $0 == character }.count
         }
-        guessed = guessed + [num]
-    }
+        .reduce(0, +)
 
-    return "A\(aCount)B\(bCount)"
+    return "A\(correctPositions)B\(correctDigits - correctPositions)"
 }
